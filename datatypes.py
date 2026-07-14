13a0183af5422aed2a3f94ca73c5e88e8fec9dfc
zain0333 <zain03337631969>
2026-07-14 14:17:37 +0500

    data types python program

---
+++ datatypes.py
@@ -0,0 +1,18 @@
+# Data Types in Python
+
+# Integer
+age = 18
+
+# Float
+height = 5.8
+
+# String
+name = "Zain"
+
+# Boolean
+is_student = True
+
+print("Age:", age)
+print("Height:", height)
+print("Name:", name)
+print("Student:", is_student)
