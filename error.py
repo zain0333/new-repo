2c006ec735b6949fa3994a4fcb08aaa29dd906c7
zain0333 <zain03337631969>
2026-07-14 14:39:21 +0500

    division python program

---
+++ error.py
@@ -0,0 +1,9 @@
+try:
+    num1 = int(input("Enter first number: "))
+    num2 = int(input("Enter second number: "))
+
+    result = num1 / num2
+    print("Result:", result)
+
+except ZeroDivisionError:
+    print("Error: You cannot divide by zero.")
